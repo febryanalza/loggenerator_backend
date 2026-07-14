51a1e02a04f992796887c8fe2498a3a3df38c911
Febryan Al Zaqri <febryanalzaqri27@gmail.com>
2026-07-14 23:57:51 +0700

    Add trustProxies middleware configuration in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         // Register SetLocale middleware to web group
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
